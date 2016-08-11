2f10fdb0b04af57ecce6f6664eb65c729da23f86
Richard Ejem <richard@ejem.cz>
2016-08-11 15:48:13 +0200

    added doc comments

--- src/Helpers/Strings.php
+++ src/Helpers/Strings.php
@@ -3,18 +3,39 @@
 namespace Instante\Helpers;
 
 use Instante\Utils\StaticClass;
-use Nette\InvalidArgumentException;
 
 class Strings
 {
     use StaticClass;
 
+    /**
+     * Returns substring of $string after last occurence of $separator.
+     *
+     * @param string $string
+     * @param string $separator
+     * @return string
+     */
     public static function afterLast($string, $separator)
     {
         $pos = mb_strrpos($string, $separator);
         return $pos === FALSE ? $string : mb_substr($string, $pos + mb_strlen($separator));
     }
 
+    /**
+     * Formats the string like sprintf, but with named arguments.
+     *
+     * Arguments format: %(name[;format])
+     * % must be escaped as %%
+     * Examples:
+     * <code>
+     * Strings::format('lorem %(foo)', ['foo' => 'ipsum']) -> returns 'lorem ipsum'
+     * Strings::format('number %(foo;.3f)', ['foo' => 9]) -> returns 'number 9.000'
+     * </code>
+     *
+     * @param string $str
+     * @param array $args
+     * @return string
+     */
     public static function format($str, array $args)
     {
         $result = '';
